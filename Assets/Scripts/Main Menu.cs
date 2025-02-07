2a0ada5aea7937a661bcbba3970e3a76f7c0a6a7
Thomas <thomas.boucard@epitech.eu>
2025-02-07 22:20:52 +0100

    feat: Implement exit button

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -6,4 +6,9 @@ public class MainMenu : MonoBehaviour
     public void PlayGame() {
         SceneManager.LoadScene("Game Scene");
     }
+
+    public void ExitGame() {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
 }
