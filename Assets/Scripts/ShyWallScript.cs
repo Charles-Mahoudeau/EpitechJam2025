da79c34ddad4d929aac076401aa414216483bd02
Charles Mahoudeau <37224242+Charles-Mahoudeau@users.noreply.github.com>
2025-02-08 17:59:04 +0100

    fix: Update shy wall script.

--- Assets/Scripts/ShyWallScript.cs
+++ Assets/Scripts/ShyWallScript.cs
@@ -1,19 +1,46 @@
+using System;
 using UnityEngine;
 
 public class ShyWallScript : MonoBehaviour
 {
     private MeshRenderer _renderer;
     private BoxCollider _collider;
+    private Camera _camera;
     
     private void Start()
     {
         _renderer = GetComponent<MeshRenderer>();
         _collider = GetComponent<BoxCollider>();
         
+        _camera = Camera.main;
+
+        if (!_camera)
+        {
+            throw new Exception("Camera not found");
+        }
+        
         SetVisible(true);
     }
 
-    public void SetVisible(bool visible)
+    private void Update()
+    {
+        var directionToPlayer = transform.position - _camera.transform.position;
+        var playerAngle = Vector3.SignedAngle(transform.forward, directionToPlayer, Vector3.up);
+        var angle = Vector3.SignedAngle(transform.forward, _camera.transform.forward, Vector3.up);
+
+        Debug.Log(angle);
+        
+        if (playerAngle < 0)
+        {
+            SetVisible(angle is <= 45 or >= 135);
+        }
+        else
+        {
+            SetVisible(angle is <= -135 or >= -45);
+        }
+    }
+
+    private void SetVisible(bool visible)
     {
         _renderer.enabled = visible;
         _collider.isTrigger = !visible;
