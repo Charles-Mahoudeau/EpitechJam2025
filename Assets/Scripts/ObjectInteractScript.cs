3d865555029e153a06d322e0f3ca79b7aa74d5bd
Charles Mahoudeau <37224242+Charles-Mahoudeau@users.noreply.github.com>
2025-02-08 17:03:54 +0100

    feat: Add lamp pickup functionality.

---
+++ Assets/Scripts/ObjectInteractScript.cs
@@ -0,0 +1,44 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectInteractScript : MonoBehaviour
+{
+    [SerializeField] private float colliderRadius = 1.0f;
+    [SerializeField] private Vector3 holdPosition;
+    [SerializeField] private Quaternion holdRotation;
+    
+    private SphereCollider _collider;
+    private GameObject _interactCanvasPrefab;
+    private GameObject _interactCanvas;
+    
+    private void Start()
+    {
+        _interactCanvasPrefab = Resources.Load<GameObject>("InteractPopup");
+        
+        _collider = transform.AddComponent<SphereCollider>();
+        _collider.center = Vector3.zero;
+        _collider.radius = colliderRadius;
+        _collider.isTrigger = true;
+        
+        _interactCanvas = Instantiate(_interactCanvasPrefab, transform.position + new Vector3(0.0f, 0.5f, 0.0f), 
+            Quaternion.identity);
+        _interactCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
+        
+        SetCanvasActive(false);
+    }
+
+    public void SetCanvasActive(bool active)
+    {
+        _interactCanvas.SetActive(active);
+    }
+
+    public Vector3 GetHoldPosition()
+    {
+        return holdPosition;
+    }
+
+    public Quaternion GetHoldRotation()
+    {
+        return holdRotation;
+    }
+}
---
+++ Assets/Scripts/PlayerInteractScript.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteractScript : MonoBehaviour
+{
+    [SerializeField] private float range = 10.0f;
+
+    private Dictionary<GameObject, ObjectInteractScript> _cache;
+    private GameObject _currentGameObject;
+    private ObjectInteractScript _currentInteractScript;
+
+    private GameObject _camera;
+    private GameObject PlayerCamera
+    {
+        get
+        {
+            if (!_camera)
+            {
+                _camera = transform.Find("Camera").gameObject;
+            }
+            return _camera;
+        }
+    }
+
+    private InputAction _interactAction;
+    
+    private void Start()
+    {
+        _cache = new Dictionary<GameObject, ObjectInteractScript>();
+
+        _interactAction = InputSystem.actions.FindAction("Interact");
+    }
+    
+    private void Update()
+    {
+        var hasHit = Physics.Raycast(PlayerCamera.transform.position, 
+            PlayerCamera.transform.forward, out var hit, range, 
+            LayerMask.GetMask("Interactable"));
+
+        if (!hasHit)
+        {
+            _currentInteractScript?.SetCanvasActive(false);
+            
+            _currentGameObject = null;
+            _currentInteractScript = null;
+            
+            return;
+        }
+
+        if (_interactAction.WasPressedThisFrame() && _currentGameObject)
+        {
+            _currentGameObject.transform.parent = transform;
+            _currentGameObject.transform.localPosition = _currentInteractScript.GetHoldPosition();
+            _currentGameObject.transform.localRotation = _currentInteractScript.GetHoldRotation();
+            return;
+        }
+
+        if (_currentGameObject == hit.transform.gameObject)
+        {
+            return;
+        }
+        
+        _currentInteractScript?.SetCanvasActive(false);
+        
+        _currentGameObject = hit.transform.gameObject;
+        _currentInteractScript = GetInteractScript(_currentGameObject);
+        
+        _currentInteractScript.SetCanvasActive(true);
+    }
+
+    private ObjectInteractScript GetInteractScript(GameObject target)
+    {
+        if (_cache.TryGetValue(target, out var script))
+        {
+            return script;
+        }
+        
+        script = target.GetComponent<ObjectInteractScript>();
+        _cache.Add(target, script);
+
+        if (!script)
+        {
+            throw new WarningException("Unable to find ObjectInteractScript on target " + target);
+        }
+        
+        return script;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(PlayerCamera.transform.position,
+            PlayerCamera.transform.position + PlayerCamera.transform.forward * range);
+    }
+}
