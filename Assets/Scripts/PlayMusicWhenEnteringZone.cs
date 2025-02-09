8490cc33bfee7eaaeecd7a61877d346475ef64ea
SuperLampadaire <esteban.peroz@epitech.eu>
2025-02-09 03:15:49 +0100

    Feat: add sounds

--- Assets/Scripts/PlayMusicWhenEnteringZone.cs
+++ Assets/Scripts/PlayMusicWhenEnteringZone.cs
@@ -4,7 +4,8 @@ public class PlayMusicWhenEnteringZone : MonoBehaviour
 {
   private bool _checkAudio;
   [SerializeField] private AudioSource clip;
-  
+  [SerializeField] private bool isPlayedOnce = true;
+  [SerializeField] private bool playOnlyAtExit = false;
   private void Start()
   {
       _checkAudio = false;
@@ -12,9 +13,18 @@ public class PlayMusicWhenEnteringZone : MonoBehaviour
 
   private void OnTriggerEnter(Collider other)
   {
+      if (other.CompareTag("Player") && !_checkAudio && !playOnlyAtExit)
+      {
+          _checkAudio = isPlayedOnce;
+          clip.Play();
+      }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
       if (other.CompareTag("Player") && !_checkAudio)
       {
-          _checkAudio = true;
+          _checkAudio = isPlayedOnce;
           clip.Play();
       }
   }
